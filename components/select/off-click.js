5c87700c384b6808e27fe2a48e4028aa6eb04e6d
Grant Lucas <contact@grantlucas.com>
2017-01-12 15:12:21 -0500

    Updated version requiremnts in package json

--- components/select/off-click.js
+++ components/select/off-click.js
@@ -1,29 +1,36 @@
-"use strict";
+'use strict';
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+  var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+  if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function') r = Reflect.decorate(decorators, target, key, desc);
+  else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+  return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+  if (typeof Reflect === 'object' && typeof Reflect.metadata === 'function') return Reflect.metadata(k, v);
+};
 var core_1 = require('@angular/core');
 var OffClickDirective = (function () {
-    function OffClickDirective() {
-    }
+  function OffClickDirective() {
+  }
+
     /* tslint:enable */
     // @HostListener('click', ['$event']) public onClick($event: MouseEvent): void {
     //   $event.stopPropagation();
     // }
-    OffClickDirective.prototype.ngOnInit = function () {
-        var _this = this;
-        setTimeout(function () { document.addEventListener('click', _this.offClickHandler); }, 0);
-    };
-    OffClickDirective.prototype.ngOnDestroy = function () {
-        document.removeEventListener('click', this.offClickHandler);
-    };
-    OffClickDirective.decorators = [
-        { type: core_1.Directive, args: [{
-                    selector: '[offClick]'
-                },] },
-    ];
-    /** @nocollapse */
-    OffClickDirective.ctorParameters = [];
-    OffClickDirective.propDecorators = {
-        'offClickHandler': [{ type: core_1.Input, args: ['offClick',] },],
-    };
-    return OffClickDirective;
+  OffClickDirective.prototype.ngOnInit = function () {
+    var _this = this;
+    setTimeout(function () { document.addEventListener('click', _this.offClickHandler); }, 0);
+  };
+  OffClickDirective.prototype.ngOnDestroy = function () {
+    document.removeEventListener('click', this.offClickHandler);
+  };
+  return OffClickDirective;
 }());
+__decorate([
+  core_1.Input('offClick'),
+  __metadata('design:type', Object)
+], OffClickDirective.prototype, 'offClickHandler', void 0);
+OffClickDirective = __decorate([
+  core_1.Directive({selector: '[offClick]'})
+], OffClickDirective);
 exports.OffClickDirective = OffClickDirective;
--- components/select/select-pipes.js
+++ components/select/select-pipes.js
@@ -1,39 +1,43 @@
-"use strict";
+'use strict';
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+  var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+  if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function') r = Reflect.decorate(decorators, target, key, desc);
+  else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+  return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 var core_1 = require('@angular/core');
 var common_1 = require('./common');
 var HighlightPipe = (function () {
-    function HighlightPipe() {
+  function HighlightPipe() {
+  }
+  HighlightPipe.prototype.transform = function (value, query) {
+    if (query.length < 1) {
+      return value;
     }
-    HighlightPipe.prototype.transform = function (value, query) {
-        if (query.length < 1) {
-            return value;
-        }
-        if (query) {
-            var tagRE = new RegExp('<[^<>]*>', 'ig');
+    if (query) {
+      var tagRE = new RegExp('<[^<>]*>', 'ig');
             // get ist of tags
-            var tagList = value.match(tagRE);
+      var tagList = value.match(tagRE);
             // Replace tags with token
-            var tmpValue = value.replace(tagRE, '$!$');
+      var tmpValue = value.replace(tagRE, '$!$');
             // Replace search words
-            value = tmpValue.replace(new RegExp(common_1.escapeRegexp(query), 'gi'), '<strong>$&</strong>');
+      value = tmpValue.replace(new RegExp(common_1.escapeRegexp(query), 'gi'), '<strong>$&</strong>');
             // Reinsert HTML
-            for (var i = 0; value.indexOf('$!$') > -1; i++) {
-                value = value.replace('$!$', tagList[i]);
-            }
-        }
-        return value;
-    };
-    HighlightPipe.decorators = [
-        { type: core_1.Pipe, args: [{ name: 'highlight' },] },
-    ];
-    /** @nocollapse */
-    HighlightPipe.ctorParameters = [];
-    return HighlightPipe;
+      for (var i = 0; value.indexOf('$!$') > -1; i++) {
+        value = value.replace('$!$', tagList[i]);
+      }
+    }
+    return value;
+  };
+  return HighlightPipe;
 }());
+HighlightPipe = __decorate([
+  core_1.Pipe({name: 'highlight'})
+], HighlightPipe);
 exports.HighlightPipe = HighlightPipe;
 function stripTags(input) {
-    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
-    var commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
-    return input.replace(commentsAndPhpTags, '').replace(tags, '');
+  var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
+  var commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+  return input.replace(commentsAndPhpTags, '').replace(tags, '');
 }
 exports.stripTags = stripTags;
