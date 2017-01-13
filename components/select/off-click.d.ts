ccf3de1ecc29bbece8ac75c665d13723957c8b31
Grant Lucas <contact@grantlucas.com>
2017-01-13 12:18:36 -0500

    AFter running npm install

---
+++ components/select/off-click.d.ts
@@ -0,0 +1,6 @@
+import { OnInit, OnDestroy } from '@angular/core';
+export declare class OffClickDirective implements OnInit, OnDestroy {
+    offClickHandler: any;
+    ngOnInit(): any;
+    ngOnDestroy(): any;
+}
