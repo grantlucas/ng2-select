42f008353fd85407cb6061860610fdbc832d8d21
Grant Lucas <contact@grantlucas.com>
2017-01-13 12:04:51 -0500

    Cleaned generated files

--- components/select/off-click.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnInit, OnDestroy } from '@angular/core';
-export declare class OffClickDirective implements OnInit, OnDestroy {
-    offClickHandler: any;
-    ngOnInit(): any;
-    ngOnDestroy(): any;
-}
